4686dfa7b62860980fe9bbea515fe9f868d44333
lismaxserg <hardy45@mail.ru>
2020-02-07 20:16:48 +0300

    Start

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.arrow'
+    },
+    breakpoints: {
+      540: {
+        slidesPerView: 2,
+      }
+    }
+      
+});
+
+
+let menuButton = document.querySelector('.menu-button');
+let menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
+}); 
